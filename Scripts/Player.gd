8653812b251f37db0a8c009e09bc3080b058da6b
puzzud <puzzud@gmail.com>
2019-04-27 15:55:35 -0400

    Made sure default rabbit animation is looping on ready.

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -6,7 +6,7 @@ extends KinematicBody
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$"Rabbit Import/AnimationPlayer".get_animation("default").loop = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
