d589dfc5238ff890812207059d3156db131b3378
puzzud <puzzud@gmail.com>
2019-04-27 20:55:09 -0400

    Animated progress bar.

--- Scripts/Ui.gd
+++ Scripts/Ui.gd
@@ -4,7 +4,14 @@ func _ready():
 	Global.ui = self
 
 func updateHealthBar(healthAmount):
-	$"Top Panel/ProgressBar".value = healthAmount
+	var initialHealthValue = $"Top Panel/ProgressBar".value
+	
+	var tween = $"Top Panel/ProgressBarValueTween"
+	tween.interpolate_property($"Top Panel/ProgressBar", "value", initialHealthValue, healthAmount, 0.33, Tween.TRANS_CIRC, Tween.EASE_OUT)
+	tween.start()
+	
+	if initialHealthValue > healthAmount:
+		$"Top Panel/ColorAnimator".play("decrement")
 
 func onPlayerDeath():
 	$"Main Caption/Dead".visible = true
