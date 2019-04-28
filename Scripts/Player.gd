5fe058b0e5da74c44db1f90c06f9680df4cfe39c
puzzud <puzzud@gmail.com>
2019-04-27 20:39:50 -0400

    Player model shrinks to nothing when dying.

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -16,6 +16,9 @@ func _physics_process(delta):
 	move_and_slide(Vector3(0,0,-5.0));
 
 func takeDamage(damageAmount):
+	if health <= 0.0:
+		return
+	
 	health -= damageAmount
 	if health <= 0.0:
 		health = 0.0
@@ -26,4 +29,6 @@ func takeDamage(damageAmount):
 	$ColorAnimator.play("hurt")
 
 func startDying():
+	$SizeAnimator.play("dying")
+	
 	Global.game.onPlayerDeath()
